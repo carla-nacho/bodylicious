8ead87e558462fa4034fdc33909e87ec7a1ecc1a
Carla Rodriguez <carlavrodriguezm@gmail.com>
2019-08-04 20:02:17 +0200

    Primer intento con API (No funciona

--- routes/diary.js
+++ routes/diary.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+//const nutrientsAPI = new APIHandler('https://api.nal.usda.gov/ndb')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
