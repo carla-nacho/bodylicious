c351e557b0cdcd52089b03d3beb3c05cb3c871ad
Nacho Alonso <narchercher@gmail.com>
2019-08-05 12:31:56 +0200

    Rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login')
 
 /* GET home page */
-router.get('/', ensureLoggedOut(), (req, res, next) => {
+router.get('/', (req, res, next) => {
 	res.render('index')
 })
 
