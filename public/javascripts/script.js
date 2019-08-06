ce7553e6bfb4822d5a0222d86e2e7bcf0ec0383a
Nacho Alonso <narchercher@gmail.com>
2019-08-06 19:56:36 +0200

    nav dissapear

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,11 @@
 document.addEventListener(
 	'DOMContentLoaded',
 	() => {
+		if (document.getElementById('start-btn')) {
+			document.getElementsByClassName('navbar')[0].classList.add('display-none')
+			document.getElementsByClassName('navbar')[1].classList.add('display-none')
+		}
+
 		console.log('IronGenerator JS imported successfully!')
 	},
 	false
