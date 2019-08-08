3b132ae9131086511197fbc58504a8fbe733a78d
Nacho Alonso <narchercher@gmail.com>
2019-08-08 12:52:44 +0200

    changes 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 
 mongoose
-	.connect(`${process.env.DB_LOCAL}`, { useNewUrlParser: true })
+	.connect(`${process.env.DB_CLOUD}`, { useNewUrlParser: true })
 	.then(x => {
 		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
 	})
